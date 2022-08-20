bcfdd610334d9bb4b6c0c574afb0fdc2fedad050
Maarten Pronk <git@evetion.nl>
2022-08-20 18:11:02 +0200

    Fixed output type bug and :Z scaling bug in reduce.

--- src/PointCloudRasterizers.jl
+++ src/PointCloudRasterizers.jl
@@ -10,37 +10,37 @@ include("utils.jl")
 """GeoArray with the number of points in each cell
 and an index for each point pointing to the cell its in."""
 struct PointCloudIndex
-	ds::LazIO.LazDataset
-	counts::GeoArray
-	index::Vector{Int64}
+    ds::LazIO.LazDataset
+    counts::GeoArray
+    index::Vector{Int64}
 end
 
 crs(ds::LazIO.LazDataset) = ""  # implement at LazIO.jl
 
 function index(ds::LazIO.LazDataset, cellsizes, bbox=LazIO.boundingbox(ds), wkt=crs(ds))
 
-	# determine requested raster size
-	unscaled_cellsizes = (cellsizes[1] / ds.header.x_scale_factor, cellsizes[2] / ds.header.y_scale_factor)
+    # determine requested raster size
+    unscaled_cellsizes = (cellsizes[1] / ds.header.x_scale_factor, cellsizes[2] / ds.header.y_scale_factor)
     indvec = zeros(Int, length(ds))
     min_x, min_y, min_z, max_x, max_y, max_z = bbox
 
-	# Scale to stored coordinates
-	u_min_x = round(Int32, (min_x - ds.header.x_offset) / ds.header.x_scale_factor)
-	u_min_y = round(Int32, (min_y - ds.header.y_offset) / ds.header.y_scale_factor)
-	u_min_z = round(Int32, (min_z - ds.header.z_offset) / ds.header.z_scale_factor)
-	u_max_x = round(Int32, (max_x - ds.header.x_offset) / ds.header.x_scale_factor)
-	u_max_y = round(Int32, (max_y - ds.header.y_offset) / ds.header.y_scale_factor)
-	u_max_z = round(Int32, (max_z - ds.header.z_offset) / ds.header.z_scale_factor)
-	unscaled_bbox = (u_min_x, u_min_y, u_min_z, u_max_x, u_max_y, u_max_z)
+    # Scale to stored coordinates
+    u_min_x = round(Int32, (min_x - ds.header.x_offset) / ds.header.x_scale_factor)
+    u_min_y = round(Int32, (min_y - ds.header.y_offset) / ds.header.y_scale_factor)
+    u_min_z = round(Int32, (min_z - ds.header.z_offset) / ds.header.z_scale_factor)
+    u_max_x = round(Int32, (max_x - ds.header.x_offset) / ds.header.x_scale_factor)
+    u_max_y = round(Int32, (max_y - ds.header.y_offset) / ds.header.y_scale_factor)
+    u_max_z = round(Int32, (max_z - ds.header.z_offset) / ds.header.z_scale_factor)
+    unscaled_bbox = (u_min_x, u_min_y, u_min_z, u_max_x, u_max_y, u_max_z)
 
     counts = countsgrid(unscaled_bbox, unscaled_cellsizes)
     linind = LinearIndices(counts)
-	@info "Indexing into a grid of $(size(counts))"
+    @info "Indexing into a grid of $(size(counts))"
 
     @showprogress "Building raster index.." for (i, p) in enumerate(ds)
-        (u_min_x < p.X <= u_max_x && u_min_y < p.Y <= u_max_y ) || continue #&& u_min_z <= p.Z <= u_max_z) || continue
-        row = Int(fld(p.X - u_min_x, unscaled_cellsizes[1])+1)
-        col = Int(fld(p.Y - u_min_y, unscaled_cellsizes[2])+1)
+        (u_min_x < p.X <= u_max_x && u_min_y < p.Y <= u_max_y) || continue #&& u_min_z <= p.Z <= u_max_z) || continue
+        row = Int(fld(p.X - u_min_x, unscaled_cellsizes[1]) + 1)
+        col = Int(fld(p.Y - u_min_y, unscaled_cellsizes[2]) + 1)
         # height = div(p.Z - min_z, cellsize_z) + 1
 
         # Include points on edge
@@ -53,25 +53,28 @@ function index(ds::LazIO.LazDataset, cellsizes, bbox=LazIO.boundingbox(ds), wkt=
         @inbounds counts[li] += 1
     end
 
-	affine = GeoArrays.geotransform_to_affine(SVector(min_x,cellsizes[1],0.,min_y,0.,cellsizes[2]))
-	ga = GeoArray(reshape(counts, size(counts)..., 1), affine, wkt)
+    affine = GeoArrays.geotransform_to_affine(SVector(min_x, cellsizes[1], 0.0, min_y, 0.0, cellsizes[2]))
+    ga = GeoArray(reshape(counts, size(counts)..., 1), affine, wkt)
 
-	PointCloudIndex(ds, ga, indvec)
+    PointCloudIndex(ds, ga, indvec)
 end
 
 """Filter out index on given condition."""
 function Base.filter!(index::PointCloudIndex, condition=nothing)
-	if condition != nothing
-	    @showprogress 1 "Reducing points..." for (i, p) in enumerate(index.ds)
-			@inbounds ind = index.index[i]
-			ind == 0 && continue  # filtered point
+    if condition != nothing
+        n = 0
+        @showprogress 1 "Reducing points..." for (i, p) in enumerate(index.ds)
+            @inbounds ind = index.index[i]
+            ind == 0 && continue  # filtered point
 
-			if ~condition(p)
-				@inbounds index.counts.A[ind] -= 1
-				@inbounds index.index[i] = 0
-			end
-		end
-	end
+            if ~condition(p)
+                @inbounds index.counts.A[ind] -= 1
+                @inbounds index.index[i] = 0
+                n += 1
+            end
+        end
+        @info "Filtered $(n) (of $(length(index.ds))) points."
+    end
 end
 
 """Filter out index on given condition relative to a reduced layer."""
@@ -83,7 +86,8 @@ function Base.filter!(index::PointCloudRasterizers.PointCloudIndex, raster::GeoA
     index.counts.crs == raster.crs || error("The crs information does not match")
 
     if condition != nothing
-        @showprogress 1 "Reducing points..." for (i, p) in enumerate(index.ds)
+        n = 0
+        @showprogress 1 "Filtering points..." for (i, p) in enumerate(index.ds)
             @inbounds ind = index.index[i]
             ind == 0 && continue  # filtered point
             raster_value = raster[ind]
@@ -91,19 +95,21 @@ function Base.filter!(index::PointCloudRasterizers.PointCloudIndex, raster::GeoA
             if ~condition(p, raster_value)
                 @inbounds index.counts.A[ind] -= 1
                 @inbounds index.index[i] = 0
+                n += 1
             end
         end
+        @info "Filtered $(n) (of $(length(index.ds))) points."
     end
 end
 
 """Reduce multiple points to single value in given indexed pointcloud."""
 function Base.reduce(index::PointCloudIndex; field::Symbol=:Z, reducer=minimum, output_type=Float64)
 
-	# Setup output grid
-	counts = copy(index.counts.A)
-	# output_type = fieldtype(eltype(index.ds), field)
-    d = Dict{Int, Vector{output_type}}()
-    output = similar(counts, Union{Missing, output_type})  # init missing
+    # Setup output grid
+    counts = copy(index.counts.A)
+    # output_type = fieldtype(eltype(index.ds), field)
+    d = Dict{Int,Vector{output_type}}()
+    output = similar(counts, Union{Missing,output_type})  # init missing
 
     @showprogress 1 "Reducing points..." for (i, p) in enumerate(index.ds)
 
@@ -128,29 +134,29 @@ function Base.reduce(index::PointCloudIndex; field::Symbol=:Z, reducer=minimum, 
         if newcnt == 0
             points = d[ind]
 
-			if length(points) > 0
-            	output[ind] = reducer(points)
-			else
-				output[ind] = missing
-			end
+            if length(points) > 0
+                output[ind] = reducer(points)
+            else
+                output[ind] = missing
+            end
 
             delete!(d, ind)
         end
     end
 
-	# Scale coordinates back if necessary
-	if field == :Z
-		output = (output .+ index.ds.header.z_offset) .* index.ds.header.z_scale_factor
-	end
+    # Scale coordinates back if necessary
+    if field == :Z && reducer != length
+        output = muladd.(output, output_type(index.ds.header.z_scale_factor), output_type(index.ds.header.z_offset))
+    end
 
-	ga = GeoArray(output, index.counts.f, index.counts.crs)
-	ga
+    ga = GeoArray(output, index.counts.f, index.counts.crs)
+    ga
 end
 
 export
-	bbox,
-	index,
-	filter,
-	reduce
+    bbox,
+    index,
+    filter,
+    reduce
 
 end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,26 +5,31 @@ using LazIO
 using GeoArrays
 using Statistics
 
-lazfn = joinpath(dirname(pathof(LazIO)), "..", "test/libLAS_1.2.laz")
-pointcloud = LazIO.open(lazfn)
+@testset "Rasterize file" begin
+    lazfn = joinpath(dirname(pathof(LazIO)), "..", "test/libLAS_1.2.laz")
+    pointcloud = LazIO.open(lazfn)
 
-cellsizes = (1.,1.)
-idx = index(pointcloud, cellsizes)
-raster = reduce(idx, field=:Z, reducer=median)
+    cellsizes = (1.0, 1.0)
+    idx = index(pointcloud, cellsizes)
+    raster = reduce(idx, field=:Z, reducer=median, output_type=Float32)
+    @test size(raster) == (5000, 5000, 1)
+    @test all(832 .< skipmissing(raster) .< 973)
+    @test all(typeof.(skipmissing(raster)) .== Float32)
 
-# indexing and reducing tests
-@test typeof(idx) == PointCloudRasterizers.PointCloudIndex
-@test count(ismissing,raster) == 24503457
-@test count(!ismissing,raster) == 496543
-@test isapprox(mean(skipmissing(raster.A)), 861.5422515270361)
-@test maximum(idx.counts) == 2
+    # indexing and reducing tests
+    @test typeof(idx) == PointCloudRasterizers.PointCloudIndex
+    @test count(ismissing, raster.A) == 24503457
+    @test count(!ismissing, raster.A) == 496543
+    @test isapprox(mean(skipmissing(raster.A)), 861.5422515270361)
+    @test maximum(idx.counts.A) == 2
 
-# filtering tests
-last_return(p) = LazIO.return_number(p) == LazIO.number_of_returns(p)
-filter!(idx, last_return)
-@test sum(idx.counts) == 497534
+    # filtering tests
+    last_return(p) = LazIO.return_number(p) == LazIO.number_of_returns(p)
+    filter!(idx, last_return)
+    @test sum(idx.counts.A) == 497534
 
-# file IO tests
-GeoArrays.write!("last_return_median.tif", raster)
-@test isfile("last_return_median.tif")
-rm("last_return_median.tif")
+    # file IO tests
+    GeoArrays.write!("last_return_median.tif", raster)
+    @test isfile("last_return_median.tif")
+    rm("last_return_median.tif")
+end
